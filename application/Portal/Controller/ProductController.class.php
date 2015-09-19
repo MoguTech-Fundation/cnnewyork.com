aa7b642dd5125cbd76ea12d544c09a0464a08c4f
炎藤 <weijingjaylin@gmail.com>
2015-09-18 18:30:30 -0700

    add product for home

---
+++ application/Portal/Controller/ProductController.class.php
@@ -0,0 +1,18 @@
+<?php
+// +----------------------------------------------------------------------
+// | TCShop [ A SIMPLE SHOP ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2015 http://www.mogutech.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: Jay Lynn <lwjct@hotmail.com>
+// +----------------------------------------------------------------------
+namespace Portal\Controller;
+use Common\Controller\HomeBaseController; 
+class ProductController extends HomeBaseController {
+	public function index() {
+    	$this->display();
+    }   
+
+}
+
+
